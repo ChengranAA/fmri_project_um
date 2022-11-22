1661a694c74186df44f08fd7d3c4f1f087219f32
ChengranAA <andrew_lcr@icloud.com>
2022-11-22 10:23:28 +0100

    add logging ability to the mock scanner

--- MockScanner/mock_scanner.py
+++ MockScanner/mock_scanner.py
@@ -2,11 +2,27 @@
 This program is used for testing the main experiment program.
 The program simulate a fmri scanner routine by constantly sending a "5" keyboard input
 """
-import time, pyautogui
+import time, pyautogui,os
 tr = 1.5 # interval in ms
+
+# create a log file for the scanner
+if not os.path.isfile('scanner_log.txt'):
+    f = open('scanner_log.txt', 'x')
+
+# delay
 time.sleep(5)
+
+# start scanner
 print('Start scanner')
+with open('scanner_log.txt', 'w') as f:
+    f.write('Scanner strat\n')
+
+counter = 0
 
 while True:
     time.sleep(tr)
     pyautogui.press('5')
+    counter += 1
+    t = time.time()
+    with open('scanner_log.txt', 'a') as f:
+        f.write("{}, {}\n".format(counter, t))
